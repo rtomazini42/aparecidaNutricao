78a3f63a7ee99747bd4713b9362599bc68e501d2
renan tomazini <renantomazini@gmail.com>
2022-06-09 19:56:47 -0300

    adições gerais

--- js/principal.js
+++ js/principal.js
@@ -18,12 +18,14 @@ for(var i = 0; i < pacientes.length; i++){
         console.log("Peso inválido");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido";
+        pacientes[i].classList.add("paciente-invalido");
     }
 
     if(altura<= 0 || altura >= 3.00){
         console.log("Altura inválido");
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida";
+        pacientes[i].classList.add("paciente-invalido");
     }
 
 
