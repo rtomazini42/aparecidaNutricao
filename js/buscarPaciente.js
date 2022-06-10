46f50a8cc8ca2fdb9e78cffb4659e4562bea0c1f
renan tomazini <renantomazini@gmail.com>
2022-06-10 17:42:18 -0300

    aula 9

---
+++ js/buscarPaciente.js
@@ -0,0 +1,15 @@
+var botaoAdicionar = document.querySelector("#buscarPaciente");
+
+botaoAdicionar.addEventListener("click", function(){
+    //event.preventDefault();
+
+    console.log("Buscando pacientes...");
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://api.hospitalexpress.com.br/pacientes");
+    xhr.addEventListener("load", function(){
+        console.log(xhr.responseText);
+        
+    });
+    //xhr.send();
+});
