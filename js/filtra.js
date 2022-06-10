6bc671fd5ccc2147d519e00f4b73c9afea58a1d5
renan tomazini <renantomazini@gmail.com>
2022-06-10 17:16:35 -0300

    Aula 08

---
+++ js/filtra.js
@@ -0,0 +1,30 @@
+var campoFiltro = document.querySelector("#filtrarTabela");
+
+campoFiltro.addEventListener("input", function(){
+    console.log(campoFiltro.value);
+
+    var pacientes = document.querySelectorAll(".paciente");
+    
+    if(this.value.length > 0){
+        console.log("Filtrando...");
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
+
+            if(!expressao.test(nome)){
+                paciente.classList.add("invisivel");
+            }else{
+                paciente.classList.remove("invisivel");
+            }
+        }
+    }else{
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+    
+
+});
